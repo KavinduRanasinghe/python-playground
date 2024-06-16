62a55a32f9d04cc0da4a17192ec9ff89c696a4b3
KavinduRanasinghe <pehesara2001@gmail.com>
2024-06-16 10:36:25 +0530

    Big O - time complexity : Day 2

--- time-complexity/big_o.py
+++ time-complexity/big_o.py
@@ -15,3 +15,26 @@ def linear_search(arr,target):
     return False
 
 #O(n^2) - Quadratic time
+#when the input size increases , the runtime increseas quadratically
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]> arr[j+1]:
+                arr[j] , arr[j+1]=arr[j+1],arr[j]
+         
+                
+#O(log n) - Logarithmic Time
+#Runtimes grow logarithmically with the size of the input data
+#Binary search drastically reduces the search time as the size of the sorted list
+def binary_search(arr, target):
+    left, right = 0, len(arr)-1
+    while left<=right:
+        mid = (left+right)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            left=mid+1
+        else:
+            left  = mid -1
+    return -1
